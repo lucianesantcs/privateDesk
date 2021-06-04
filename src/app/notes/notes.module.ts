5f6aef596b42d8ed09acdd4809ec7ca575a04c7a
Luciane Santos <lucianesantcs@gmail.com>
2021-06-04 17:02:12 -0300

    feat: criado componente note (para exibir todas as notas)

--- src/app/notes/notes.module.ts
+++ src/app/notes/notes.module.ts
@@ -5,11 +5,12 @@ import { AddNotesComponent } from './add-notes/add-notes.component';
 import { PrivateNotesComponent } from './private-notes/private-notes.component';
 import { NotesComponent } from './notes.component';
 import { NotesHeaderComponent } from './components/notes-header/notes-header.component';
+import { NoteComponent } from './components/note/note.component';
 import { NotesRoutingModule } from './notes.routing.module';
 
 @NgModule({
-  declarations: [AddNotesComponent, PrivateNotesComponent, NotesHeaderComponent, NotesComponent],
+  declarations: [AddNotesComponent, PrivateNotesComponent, NotesHeaderComponent, NotesComponent, NoteComponent],
   imports: [CommonModule, NotesRoutingModule, FormsModule],
-  exports: [NotesComponent, AddNotesComponent, PrivateNotesComponent]
+  exports: [NotesComponent, PrivateNotesComponent]
 })
 export class NotesModule {}
