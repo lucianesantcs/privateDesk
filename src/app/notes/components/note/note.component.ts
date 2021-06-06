17c2978c8eb00d747175e59709915cd27dd31a49
Luciane Santos <lucianesantcs@gmail.com>
2021-06-06 17:08:40 -0300

    feat: passado dados de notas para ser renderizado no componente note

--- src/app/notes/components/note/note.component.ts
+++ src/app/notes/components/note/note.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Notes } from '../../notes.model';
 
 @Component({
   selector: 'app-note',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./note.component.css']
 })
 export class NoteComponent implements OnInit {
+
+  @Input() notes: Notes;
 
   constructor() { }
 
--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Notes } from './notes.model';
+import { NotesService } from './notes.service';
 
 @Component({
   selector: 'app-notes',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotesComponent implements OnInit {
 
-  constructor() { }
+  notes: Notes;
+
+  constructor(private notesService: NotesService) { }
 
   ngOnInit(): void {
+    this.notesService.getNotes().subscribe(note => this.notes = note)
   }
 
 }
