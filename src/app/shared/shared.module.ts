afc5943477f57c9e7ea2f23b5238f0ca7b7a51a1
Luciane Santos <lucianesantcs@gmail.com>
2021-06-29 15:47:01 -0300

    feat: criado componente botão e ajustado nas páginas login e cadastro

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { FieldErrorMessageComponent } from './components/field-error-message/field-error-message.component';
 import { HeaderComponent } from './components/header/header.component';
-import { RouterModule } from '@angular/router';
+import { ButtonComponent } from './components/button/button.component';
 
 
 
 @NgModule({
-  declarations: [FieldErrorMessageComponent, HeaderComponent],
-  exports: [FieldErrorMessageComponent, HeaderComponent],
+  declarations: [FieldErrorMessageComponent, HeaderComponent, ButtonComponent],
+  exports: [FieldErrorMessageComponent, HeaderComponent, ButtonComponent],
   imports: [
     CommonModule, RouterModule
   ]
